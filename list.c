36200b7c22e201e1cb7fd800e2a04ad85e637a73
Replit user <>
2022-08-19 13:20:30 +0000

    new try

--- list.c
+++ list.c
@@ -37,7 +37,9 @@ List * createList() {
 }
 
 void * firstList(List * list) {
-    return NULL;
+  if (!list->first) return NULL;
+  list->current=list->first;
+  return (list->first->data);
 }
 
 void * nextList(List * list) {
