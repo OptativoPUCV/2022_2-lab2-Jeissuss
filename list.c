b567458bf836965db7d8af8242aef74603b52919
Replit user <>
2022-08-21 18:30:07 +0000

    new try

--- list.c
+++ list.c
@@ -85,7 +85,7 @@ void pushBack(List * list, void * data) {
 
 void pushCurrent(List * list, void * data) {
   Node *newNode = createNode(data);
-  if (list->current == lsit->tail){
+  if (list->current == list->tail){
     newNode->prev = list->current;
     list->current->next = newNode:
     list->tail = newNode;
